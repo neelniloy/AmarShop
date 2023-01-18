9055de151ba40c7239f91f584b035df9a03273ee
Niloy Sarker <niloy64529@gmail.com>
2023-01-18 21:13:44 +0600

    initial commit

--- app/src/main/java/com/braineer/amarshopbyseo/utils/functions.kt
+++
@@ -1,6 +0,0 @@
-
-import java.text.SimpleDateFormat
-import java.util.*
-
-fun getFormattedDateTime(millis: Long, format: String) =
-    SimpleDateFormat(format, Locale.getDefault()).format(Date(millis))
